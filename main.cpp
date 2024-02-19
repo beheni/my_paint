a377ddb6318e48a7ca2ca0f1a670e554670db9aa
redn1njaa <ostap.seryvko@ucu.edu.ua>
2024-02-19 12:27:20 +0200

    helloworld

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-#include <iostream>
+#include <QApplication>
+#include <QPushButton>
 
-int main(int argc, char* argv[]) {
-    std::cout << "A flag value: ";
-    return 0;
+int main(int argc, char **argv)
+{
+    QApplication app (argc, argv);
+
+    QPushButton button;
+    button.setText("My text");
+    button.setToolTip("A tooltip");
+    button.show();
+
+    return QApplication::exec();
 }
