07261165cc1a98e576085e7ca63994f7c6eb3cbe
beheni <anastasiia.beheni@ucu.edu.ua>
2024-04-29 23:20:48 +0300

    very simple drawing

--- include/Canvas.h
+++ include/Canvas.h
@@ -1,11 +1,23 @@
 #ifndef MY_PAINT_CANVAS_H
 #define MY_PAINT_CANVAS_H
 
+#include <vector>
 #include <QWidget>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QPainterPath>
 
 class Canvas: public QWidget {
     Q_OBJECT
 public:
     Canvas(QWidget *parent = nullptr);
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+private:
+    bool drawing;
+    QPainterPath path;
 };
 #endif //MY_PAINT_CANVAS_H
--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -1,6 +1,37 @@
 #include "Canvas.h"
 
 Canvas::Canvas(QWidget *parent) : QWidget(parent) {
-    setStyleSheet("background-color: black;");
-    setMinimumSize(400, 400);
+    setMouseTracking(true);
+    setStyleSheet("background-color: white;");
 }
+
+void Canvas::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+    painter.setBrush(Qt::NoBrush);
+    painter.drawPath(path);
+}
+
+void Canvas::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        path.moveTo(event->pos());
+        drawing = true;
+    }
+}
+
+void Canvas::mouseMoveEvent(QMouseEvent *event) {
+    if ((event->buttons() & Qt::LeftButton) && drawing) {
+        path.lineTo(event->pos());
+        update();
+    }
+}
+
+void Canvas::mouseReleaseEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton && drawing) {
+        path.lineTo(event->pos());
+        drawing = false;
+        update();
+    }
+}
+
