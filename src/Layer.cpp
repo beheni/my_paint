4eed79800e4c563fde3806aec9f4a445cf87714c
redn1njaa <ostap.seryvko@ucu.edu.ua>
2024-05-20 00:21:42 +0300

    smol

--- src/Layer.cpp
+++ src/Layer.cpp
@@ -5,3 +5,15 @@ int Layer::count = 0;
 Layer::Layer(QGraphicsItem* parent): QGraphicsItemGroup(parent){
     name = "Layer" + QString::number(count++);
 }
+
+void Layer::addBatch(QList<QGraphicsItem*> items){
+    for (auto item: items){
+        addToGroup(item);
+    }
+}
+
+void Layer::removeBatch(QList<QGraphicsItem*> items){
+    for (auto item: items){
+        removeFromGroup(item);
+    }
+}
